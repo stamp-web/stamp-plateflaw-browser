9b2b4379ff73e0b236173ea0b7bdc334741e96dd
jadrake75 <jadrake75@gmail.com>
2023-12-20 16:21:16 -0500

    Make screens more mobile friendly

--- src/views/ImageBrowser.vue
+++ src/views/ImageBrowser.vue
@@ -53,18 +53,28 @@ onMounted(async () => {
 
 <template>
   <div class="flew flex-col w-full overflow-y-hidden flex-grow-0">
-    <div class="h-[50px] p-2 bg-gray-900 text-gray-100 border-b-2 border-green-500 flex">
-      <img alt="logo" class="w-8 h-8 mr-1" src="../assets/images/stamp-web-64x64.png" />
-      <h1 class="text-2xl truncate">Stamp Web Editor: Plate Flaws</h1>
+    <div
+      class="md:h-[50px] h-[2rem] md:p-2 p-1 bg-gray-900 text-gray-100 border-b-2 border-green-500 flex align-middle"
+    >
+      <img
+        alt="logo"
+        class="md:w-8 md:h-8 w-6 h-6 mr-1"
+        src="../assets/images/stamp-web-64x64.png"
+      />
+      <h1 class="md:text-2xl text-md truncate">Stamp Web Editor: Plate Flaws</h1>
     </div>
-    <div class="flex absolute top-[50px] right-0 left-0 bottom-0 p-2">
-      <div class="flex flex-col w-[350px]">
-        <div class="flex items-center align-middle text-center p-1 mb-1">
+    <div
+      class="flex md:flex-row flex-col absolute md:top-[50px] top-[2rem] right-0 left-0 bottom-0 p-2"
+    >
+      <div
+        class="flex flex-col md:w-[350px] md:h-full md:max-h-full w-full max-h-[200px] md:mb-0 mb-2"
+      >
+        <div class="flex items-center align-middle text-center p-1 mb-1 text-sm">
           Filter:
           <input
             type="text"
             placeholder="Search by"
-            class="border ml-2 rounded p-1"
+            class="border ml-2 rounded p-1 text-sm"
             v-model="filterText"
             @change="filter()"
           />
@@ -72,13 +82,13 @@ onMounted(async () => {
             X
           </button>
         </div>
-        <div class="overflow-y-auto flex flex-col w-[350px] border">
+        <div class="overflow-y-auto flex flex-col border">
           <li v-for="flaw in filteredFlaws" :key="flaw.name" class="list-none">
             <span
               @click="showFlaw(flaw)"
               :class="`${
                 flaw === currentFlaw ? 'font-bold bg-green-500 text-white' : ''
-              } hover:bg-blue-500 hover:text-white w-full flex p-2 active:bg-blue-700`"
+              } hover:bg-blue-500 hover:text-white w-full flex p-2 active:bg-blue-700 text-sm`"
               >{{ flaw.name }}</span
             >
           </li>
@@ -87,7 +97,7 @@ onMounted(async () => {
 
       <div class="w-full flex-grow">
         <div class="flex flex-col overflow-y-hidden w-full h-full bg-gray-900">
-          <div class="bg-white p-2" v-if="currentFlaw">
+          <div class="bg-white p-2 text-sm hidden md:flex" v-if="currentFlaw">
             <span class="font-bold">Filepath:</span> {{ currentFlaw.path }}
           </div>
           <div class="flex min-h-[min-content] align-middle justify-center m-auto bg-transparent">
